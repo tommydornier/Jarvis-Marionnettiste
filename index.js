27b4cc5d5b11715054509dd95d287437c76d1577
tommydornier <tommydornier77@gmail.com>
2025-02-01 17:25:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-const puppeteer = require('puppeteer');
 const express = require('express');
+const puppeteer = require('puppeteer');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
-    const browser = await puppeteer.launch({ 
-        headless: 'new',
-        args: ['--no-sandbox', '--disable-setuid-sandbox'] 
-    });
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
-
+    
     await page.goto('https://www.vinted.fr/login');
-
+    
     console.log('Page ouverte sur Vinted');
-
+    
     await browser.close();
     res.send('Puppeteer a ouvert Vinted avec succès !');
 });
 
 app.listen(PORT, () => {
-    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+    console.log(`Serveur démarré sur le port ${PORT}`);
 });
