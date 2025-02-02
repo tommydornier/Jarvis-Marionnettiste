4eccf2a717aa8a133ca91e66f78227a2779dc0ea
tommydornier <tommydornier77@gmail.com>
2025-02-02 14:45:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
-    const browser = await puppeteer.launch({ 
-        headless: "new",  // Correction ici
-        args: ['--no-sandbox', '--disable-setuid-sandbox'] 
-    });
+    try {
+        const browser = await puppeteer.launch({ 
+            headless: "new", // Correction ici 
+            args: ['--no-sandbox', '--disable-setuid-sandbox'] 
+        });
+        const page = await browser.newPage();
+        
+        await page.goto('https://www.vinted.fr/login', { timeout: 60000 });
 
-    const page = await browser.newPage();
-    
-    await page.goto('https://www.vinted.fr/login');
-    
-    console.log('Page ouverte sur Vinted');
-    
-    await browser.close();
-    res.send('Puppeteer a ouvert Vinted avec succès !');
+        console.log('Page ouverte sur Vinted');
+        
+        await browser.close();
+        res.send('Puppeteer a ouvert Vinted avec succès !');
+    } catch (error) {
+        console.error("Erreur lors de l'ouverture de la page :", error);
+        res.status(500).send("Erreur interne du serveur");
+    }
 });
 
 app.listen(PORT, () => {
